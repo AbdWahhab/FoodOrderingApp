948c69f5e53e444981b0bb9465ada28d375e7188
AbdWahhab <alphagraphic1000@gmail.com>
2026-03-12 23:47:19 +0530

    Food list UI updated

--- app/data/foodData.ts
+++ app/data/foodData.ts
@@ -23,22 +23,106 @@ export const categories = [
   
   export const foodItems = [
     // Pizza
-    { id: '101', categoryId: '1', name: 'Margherita', price: 12.99, image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=400' },
-    { id: '102', categoryId: '1', name: 'Pepperoni', price: 14.99, image: 'https://images.unsplash.com/photo-1628840042765-356cda07504e?w=400' },
-    { id: '103', categoryId: '1', name: 'BBQ Chicken', price: 15.99, image: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=400' },
+    {
+      id: '101',
+      categoryId: '1',
+      name: 'Margherita',
+      portion: 'Regular',
+      price: 1450,
+      image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=400',
+    },
+    {
+      id: '102',
+      categoryId: '1',
+      name: 'Pepperoni',
+      portion: 'Large',
+      price: 2200,
+      image: 'https://images.unsplash.com/photo-1628840042765-356cda07504e?w=400',
+    },
+    {
+      id: '103',
+      categoryId: '1',
+      name: 'BBQ Chicken',
+      portion: 'Large',
+      price: 2450,
+      image: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=400',
+    },
   
     // Burger
-    { id: '201', categoryId: '2', name: 'Classic Burger', price: 9.99, image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400' },
-    { id: '202', categoryId: '2', name: 'Cheese Burger', price: 10.99, image: 'https://images.unsplash.com/photo-1553979459-d2229ba7433b?w=400' },
-    { id: '203', categoryId: '2', name: 'Veggie Burger', price: 8.99, image: 'https://images.unsplash.com/photo-1520072959219-c595dc870360?w=400' },
+    {
+      id: '201',
+      categoryId: '2',
+      name: 'Classic Burger',
+      portion: 'Single Patty',
+      price: 750,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400',
+    },
+    {
+      id: '202',
+      categoryId: '2',
+      name: 'Cheese Burger',
+      portion: 'Double Patty',
+      price: 1100,
+      image: 'https://images.unsplash.com/photo-1553979459-d2229ba7433b?w=400',
+    },
+    {
+      id: '203',
+      categoryId: '2',
+      name: 'Veggie Burger',
+      portion: 'Single Patty',
+      price: 650,
+      image: 'https://images.unsplash.com/photo-1520072959219-c595dc870360?w=400',
+    },
   
     // Drinks
-    { id: '301', categoryId: '3', name: 'Coca Cola', price: 2.99, image: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?w=400' },
-    { id: '302', categoryId: '3', name: 'Orange Juice', price: 3.99, image: 'https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?w=400' },
-    { id: '303', categoryId: '3', name: 'Milkshake', price: 4.99, image: 'https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=400' },
+    {
+      id: '301',
+      categoryId: '3',
+      name: 'Coca Cola',
+      portion: '400ml Can',
+      price: 220,
+      image: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?w=400',
+    },
+    {
+      id: '302',
+      categoryId: '3',
+      name: 'Orange Juice',
+      portion: '350ml Fresh',
+      price: 380,
+      image: 'https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?w=400',
+    },
+    {
+      id: '303',
+      categoryId: '3',
+      name: 'Milkshake',
+      portion: '500ml Large',
+      price: 550,
+      image: 'https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=400',
+    },
   
     // Desserts
-    { id: '401', categoryId: '4', name: 'Chocolate Cake', price: 6.99, image: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=400' },
-    { id: '402', categoryId: '4', name: 'Ice Cream', price: 4.99, image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b?w=400' },
-    { id: '403', categoryId: '4', name: 'Cheesecake', price: 5.99, image: 'https://images.unsplash.com/photo-1533134242443-d4fd215305ad?w=400' },
+    {
+      id: '401',
+      categoryId: '4',
+      name: 'Chocolate Cake',
+      portion: '1 Slice',
+      price: 480,
+      image: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=400',
+    },
+    {
+      id: '402',
+      categoryId: '4',
+      name: 'Ice Cream',
+      portion: '2 Scoops',
+      price: 350,
+      image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b?w=400',
+    },
+    {
+      id: '403',
+      categoryId: '4',
+      name: 'Cheesecake',
+      portion: '1 Slice',
+      price: 520,
+      image: 'https://images.unsplash.com/photo-1533134242443-d4fd215305ad?w=400',
+    },
   ];
